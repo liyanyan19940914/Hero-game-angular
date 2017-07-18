434f107f6db86314832faa64cf20edf3678e9f43
liyanyan19940914 <921258351@qq.com>
2017-07-18 15:11:50 +0800

    add to router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}      from '@angular/forms';
+import {RouterModule}     from '@angular/router'
 
 import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from './hero-details.component';
+import {HeroesComponent} from './heroes.component'
+import {HeroService} from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule ,FormsModule],
-  declarations: [ AppComponent,HeroDetailComponent],
+  imports:      [ BrowserModule ,FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'heroes',
+        component:HeroesComponent
+      }
+    ])],
+  declarations: [ AppComponent,HeroDetailComponent,HeroesComponent],
+  providers:[HeroService],
   bootstrap:    [ AppComponent ],
 })
+
+
 export class AppModule { }
